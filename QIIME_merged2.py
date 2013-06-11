a5c02418e236b091485d57c5acf406d25463ae6c
jjoyner07 <jjoyner07@gmail.com>
2013-06-11 18:14:24 -0400

    RemovedLineSpaces

---
+++ QIIME_merged2.py
@@ -0,0 +1,4 @@
+#!/usr/local/python/2.7.2/bin/python
+import os, sys
+python2.7 usr/local/python/2.7.2/bin/merge_otu_tables.py -i file, home/ekllab/jljoyner/analysis_749749_fastq/Data/OTUs/MK_merged_otus2.biom   -o home/ekllab/jljoyner/analysis_749749_fastq/Data/OTUs/MK_merged_otus2.biom
+
