859285602ff5b3371e69a94b4d89deaf95674099
jjoyner07 <jjoyner07@gmail.com>
2013-06-12 09:55:06 -0400

    QAQCpipeline-trim
    Added in Trimomatic and removed the fastx adaptor trim

--- IlluminaQC.sh
+++ IlluminaQC.sh
@@ -1,16 +1,19 @@
 #! /bin/bash
-cd /usr/local/fastx/latest
+
 FILES=/home/ekllab/jljoyner/analysis_749749_fastq/Data/Intensities/Run1/*
 for file in $FILES
 do
-/usr/local/fastx/latest/bin/fastx_clipper -a  AATGATACGGCGACCACCGAGATCTACAC -c -l 50 -i "$file" -o "$file.Trimmed" -Q33
+time java -jar /usr/local/trimmomatic/0.27/trimmomatic-0.27.jar SE -threads 4 -phred33 -trimlog S50-trimlog "$file" "$file-QualTrim" ILLUMINACLIP:/home/ekllab/jljoyner/analysis_749749_fastq/Illumina-CapAdaptor.fasta:2:30:10 TRAILING:3 SLIDINGWINDOW:4:15 MINLEN:100
 done
-FILES=/home/ekllab/jljoyner/analysis_749749_fastq/Data/Intensities/Run1/*.Trimmed
+
+cd /usr/local/fastx/latest
+
+FILES=/home/ekllab/jljoyner/analysis_749749_fastq/Data/WorkingSeqs/*-QualTrim
 for file in $FILES
 do
-/usr/local/fastx/latest/bin/fastq_quality_filter -q 20 -p 95  -i "$file" -o "$file.QualFiltered" -Q33
+/usr/local/fastx/latest/bin/fastq_quality_filter -q 20 -p 95  -i "$file" -o "$file-QualFilt" -Q33
 done
-FILES=/home/ekllab/jljoyner/analysis_749749_fastq/Data/Intensities/Run1/*.QualFiltered
+FILES=/home/ekllab/jljoyner/analysis_749749_fastq/Data/Intensities/Run1/*.QualFilt
 for file in $FILES
 do
 /usr/local/fastx/latest/bin/fastq_to_fasta -v -i "$file" -o "$file.fasta" -Q33
