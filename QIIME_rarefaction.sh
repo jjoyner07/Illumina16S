9a01c100b46fe4164787f915775ca89583e75818
jjoyner07 <jjoyner07@gmail.com>
2013-06-11 22:37:21 -0400

    Rarefaction-ChangedSampleFile
    Changed which sample the curve is made on/for

--- QIIME_rarefaction.sh
+++ QIIME_rarefaction.sh
@@ -1,4 +1,4 @@
 #! /bin/bash
 export QIIME_CONFIG_FP=$HOME/.qiime_config
 export PATH=${PATH}:/usr/local/python/2.7.2/bin
-python2.7 /usr/local/python/2.7.2/bin/single_rarefaction.py -i /home/ekllab/jljoyner/analysis_749749_fastq/Data/OTUs/biom/S15-otu_table.biom -o /home/ekllab/jljoyner/analysis_749749_fastq/Data/Rarefaction/S15-MR07H20-911
+python2.7 /usr/local/python/2.7.2/bin/single_rarefaction.py -i /home/ekllab/jljoyner/analysis_749749_fastq/Data/OTUs/biom/S16-otu_table.biom -o /home/ekllab/jljoyner/analysis_749749_fastq/Data/Rarefaction/S16-MR07H-911
