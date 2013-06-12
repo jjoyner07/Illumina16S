685d76973fddcb79dfc5b1c5c343b25ce2a3aaec
jjoyner07 <jjoyner07@gmail.com>
2013-06-12 14:23:35 -0400

    Trimomatic-CreatedDirLoop
    Changed the script to use all the files in the directory -this edit is
    only a test with the NEG samples

--- Ilumina_Trimmomatic.sh
+++ Ilumina_Trimmomatic.sh
@@ -1,2 +1,6 @@
 #!/bin/bash
-time java -jar /usr/local/trimmomatic/0.27/trimmomatic-0.27.jar SE -threads 4 -phred33 -trimlog S50-trimlog /home/ekllab/jljoyner/analysis_749749_fastq/Data/Intensities/Run1/MK1_S50_L001_R1_001.fastq /home/ekllab/jljoyner/analysis_749749_fastq/Data/WorkingSeqs/MK1_S50_QualTrim.fastq ILLUMINACLIP:/home/ekllab/jljoyner/analysis_749749_fastq/Illumina-CapAdaptor.fasta:2:30:10 TRAILING:3 SLIDINGWINDOW:4:15 MINLEN:100
+FILES=/home/ekllab/jljoyner/analysis_749749_fastq/Data/Intensities/Run1/NEG*
+for file in $FILES
+do
+time java -jar /usr/local/trimmomatic/0.27/trimmomatic-0.27.jar SE -threads 4 -phred33 -trimlog '$file-S50-trimlog' '$file' /home/ekllab/jljoyner/analysis_749749_fastq/Data/WorkingSeqs/'$file-QualTrim' ILLUMINACLIP:/home/ekllab/jljoyner/analysis_749749_fastq/Illumina-CapAdaptor.fasta:2:30:10 TRAILING:3 SLIDINGWINDOW:4:15 MINLEN:100
+done
