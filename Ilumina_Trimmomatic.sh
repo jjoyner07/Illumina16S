7c8cff0e57ea78d74a2abfe33ad60df2ebe79504
jjoyner07 <jjoyner07@gmail.com>
2013-06-12 15:27:44 -0400

    Trim-SingleToDoubleQuotes
    Exchanged the single quotes to double quotes within the for loop

--- Ilumina_Trimmomatic.sh
+++ Ilumina_Trimmomatic.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
-FILES=/home/ekllab/jljoyner/analysis_749749_fastq/Data/Intensities/Run1/NEG*
+FILES=/home/ekllab/jljoyner/analysis_749749_fastq/Data/Intensities/Test/NEG*
 for file in $FILES
 do
-time java -jar /usr/local/trimmomatic/0.27/trimmomatic-0.27.jar SE -threads 4 -phred33 -trimlog '$file-S50-trimlog' '$file' /home/ekllab/jljoyner/analysis_749749_fastq/Data/WorkingSeqs/'$file-QualTrim' ILLUMINACLIP:/home/ekllab/jljoyner/analysis_749749_fastq/Illumina-CapAdaptor.fasta:2:30:10 TRAILING:3 SLIDINGWINDOW:4:15 MINLEN:100
+java -jar /usr/local/trimmomatic/0.27/trimmomatic-0.27.jar SE -threads 4 -phred33 -trimlog "$file-trimlog" "$file" /home/ekllab/jljoyner/analysis_749749_fastq/Data/WorkingSeqs/"$file-QualTrim" ILLUMINACLIP:/home/ekllab/jljoyner/analysis_749749_fastq/Illumina-CapAdaptor.fasta:2:30:10 TRAILING:3 SLIDINGWINDOW:4:15 MINLEN:100
 done
